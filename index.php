ade42d1623bc6fb6f9285c4ad1438875e47d59b7
rock1234rock <59310502@go.buu.ac.th>
2020-03-24 10:29:40 +0700

    new

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD><TITLE> My Homepage </TITLE></HEAD>
+<BODY BGCOLOR=#FFFFFF>
+<H1>
+<?php echo "รหัสนิสิต-59310502"; ?><br>
+<?php echo "ชื่อ-สกุล นาย ศรัณย์  โภคสมบัติ"; ?><br>
+<iframe width="500" height="500"
+src="https://youtu.be/2_0JZT3lRyg">
+</iframe>
+</BODY>
+</HTML>
