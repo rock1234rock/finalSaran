ee325868fe5dff9fba130c3c4031cf1bcf3d4cb5
Saran Poksombat <59908561+rock1234rock@users.noreply.github.com>
2020-03-24 10:50:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <H1>
 <?php echo "รหัสนิสิต-59310502"; ?><br>
 <?php echo "ชื่อ-สกุล นาย ศรัณย์  โภคสมบัติ"; ?><br>
-<iframe width="420" height="345" src="
-https://www.youtube.com/watch?v=2_0JZT3lRyg&feature=youtu.be&fbclid=IwAR3U76GNM7l1w0wDkdN4YChCCQEegb1B2eTn7BZ5idpn5KlR0CfTEg30S50">
+<iframe width="500" height="500"
+src="https://youtu.be/2_0JZT3lRyg">
 </iframe>
 </BODY>
 </HTML>
